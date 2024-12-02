0c3869eefee4ee232467a68180db9b8235ae268b
Jon Seager <jon@sgrs.uk>
2024-12-02 17:57:50 +0000

    test: make Etag/If-None-Match test more robust

--- tests/assets.rs
+++ tests/assets.rs
@@ -52,12 +52,17 @@ async fn test_serve_file_by_path_if_none_match() {
     let server = mock_redirect_source().await;
     let app = servy(format!("{}{}", server.url(), "/mock_redirects").as_str());
 
+    let response = app.get("/css/main.css").await;
+    let etag = response
+        .header("ETag")
+        .to_owned()
+        .to_str()
+        .unwrap()
+        .to_string();
+
     let response = app
         .get("/css/main.css")
-        .add_header(
-            http::header::IF_NONE_MATCH,
-            "c9ebb82d93690b8747857f01b0143a9dc52c151cc6247714ae84031fbfde6181",
-        )
+        .add_header(http::header::IF_NONE_MATCH, etag)
         .await;
 
     response.assert_status(StatusCode::NOT_MODIFIED);
